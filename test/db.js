ec8cf1222fed02572d04a90dacf406a5ef057526
Glynn Bird <glynn.bird@gmail.com>
2018-01-09 15:27:15 +0000

    added `designdocs` function to fetch all design docs from a database

--- test/db.js
+++ test/db.js
@@ -16,6 +16,7 @@ describe('db', function() {
     assert(typeof db.create, 'function');
     assert(typeof db.info, 'function');
     assert(typeof db.list, 'function');
+    assert(typeof db.designdocs, 'function');
     assert(typeof db.query, 'function');
     assert(typeof db.insert, 'function');
     assert(typeof db.update, 'function');
@@ -221,6 +222,29 @@ describe('db', function() {
       assert.equal(data.length, 3);
       assert.equal(typeof data[0], 'object');
       assert.equal(data[0]._id, '1');
+      assert.equal(typeof data[0]._rev, 'undefined');
+      assert.equal(data[0].a, '1');
+      assert(mocks.isDone());
+    }).catch(function(err) {
+      assert(false);
+    });
+  });
+
+  it('designdocs - should get all design documents', function() {
+    var reply = {
+      total_rows: 3,
+      rows: [
+        { id: '_design/1', key: '_design/1', value: { rev: '1-123'}, doc: { _id: '_design/1', _rev:'1-123', a:1}},
+        { id: '_design/2', key: '_design/2', value: { rev:'1-234'}, doc: { _id: '_design/2', _rev:'1-234', a:2}},
+        { id: '_design/3', key: '_design/3', value: { rev:'1-456'}, doc: { _id: '_design/3', _rev:'1-456', a:3}},
+      ]
+    };
+    var mocks = nock(SERVER)
+      .get('/mydb/_all_docs?include_docs=true&startkey=%22_design%22&endkey=%22_design0%22&inclusive_end=false').reply(200, reply);
+    return nosql.designdocs().then(function(data) {
+      assert.equal(data.length, 3);
+      assert.equal(typeof data[0], 'object');
+      assert.equal(data[0]._id, '_design/1');
       assert.equal(typeof data[0]._rev, 'undefined');
       assert.equal(data[0].a, '1');
       assert(mocks.isDone());
